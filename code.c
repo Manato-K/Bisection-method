13ee0ca512a6414a4bbaa0d5530970b95ed4e4a1
chrom-P <kkllmm104@gmail.com>
2021-12-22 10:15:10 +0900

    up

--- code.c
+++ code.c
@@ -5,3 +5,22 @@ double f(double x)
 {
     return(x*x-2.0);
 }
+int main(void){
+    int n=0;
+    double a,b,c,fa,fc,er=1.0e-9;
+    a=0.0;
+    b=2.0;
+    do {
+        fa=f(a);
+        c=(a+b)/2.0;
+        fc=f(c);
+        if(fa*fc<0.0) {
+            b=c;
+        } else {
+            a=c;
+        }
+    n++;
+    printf("%2d : %.10lf(%.10lf)\n",n,c,fc);
+    if(fabs(fc)<er) break;
+    } while(fabs(a-b)>er);
+}
