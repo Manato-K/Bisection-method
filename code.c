a759167386bfc2bdeeba78025d325cc5e4e75c20
chrom-P <kkllmm104@gmail.com>
2022-01-11 10:31:05 +0900

    main

--- code.c
+++ code.c
@@ -4,23 +4,23 @@
 
 double f(double x)
 {
-    return(pow(x,3) * a - pow(x,2) * b - pow(x,1) * c + d);
+    return(x*x-2.0);
 }
 int main(void){
     int n=0;
-    double a,b,c,d,fa,fc,er=1.0e-9;
+    double a,b,c,d,fa,fc,fd,er=1.0e-9;
     a=256.0;
     b=320.0;
     c=308.0;
     d=147.0;
     do {
         fa=f(a);
-        c=(a+b)/2.0;
-        fc=f(c);
-        if(fa*fc<0.0) {
-            b=c;
+        d=(a+b+c)/2.0;
+        fd=f(d);
+        if(fa*fd<0.0) {
+            b=d;
         } else {
-            a=c;
+            a=d;
         }
     n++;
     printf("%2d : %.10lf(%.10lf)\n",n,c,fc);
